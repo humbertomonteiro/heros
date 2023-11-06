5b9db963fe70c04f1aef9bf548cb11c6fd96b226
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-11-06 20:28:55 -0300

    att

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const URL =
-  "https://devcenter.heroku.com/changelog-items/1815/http://homologacao3.azapfy.com.br/api/ps/metahumans";
+const URL = "http://homologacao3.azapfy.com.br/api/ps/metahumans";
 
 const api = axios.create({
   baseURL: URL,
